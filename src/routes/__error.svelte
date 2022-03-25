8d7ea324693a7cc08c83d42aedfb794a0ae15ee0
anand2312 <40204976+anand2312@users.noreply.github.com>
2022-03-25 23:30:27 +0400

    Make projects page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,28 @@
+<script context="module">
+  export function load({ error, status }) {
+    return {
+      props: {
+        title: `${status}: ${error.message}`
+      }
+    };
+  }
+</script>
+
+<script>
+  import Navbar from '$lib/components/Navbar.svelte';
+
+  export let title;
+</script>
+
+<main>
+  <Navbar />
+  <center class="flex flex-row justify-center content-center">
+    <h1 class="font-bold py-56 font-display text-pink-50 text-2xl md:text-4xl m-2 p-1">{title}</h1>
+    <img
+      src="favicon-1.webp"
+      class="py-40 md:py-0 w-1/3 h-1/3 md:w-2/4 md:h-2/3"
+      alt="sad cat as you've reached an error page"
+      type="image/webp"
+    />
+  </center>
+</main>
--- src/routes/projects.svelte
+++ src/routes/projects.svelte
@@ -1,39 +1,71 @@
+<script context="module">
+  export async function load({ params, fetch, session, stuff }) {
+    const url = 'https://site-backend-api.an23.workers.dev/pinned';
+    const response = await fetch(url);
+    const data = parseResponse(await response.json());
+
+    return {
+      status: response.status,
+      props: {
+        repos: data
+      }
+    };
+  }
+
+  function parseResponse(res) {
+    let data = res['data'];
+    let out = new Array();
+
+    for (const repo of data) {
+      out.push({
+        url: repo.url,
+        name: repo.nameWithOwner,
+        description: repo.description,
+        langColor: repo.primaryLanguage.color,
+        langName: repo.primaryLanguage.name,
+        stars: repo.stargazerCount
+      });
+    }
+    return out;
+  }
+</script>
+
 <script lang="ts">
   import Head from '$lib/components/Head.svelte';
   import Navbar from '$lib/components/Navbar.svelte';
   import Project from '$lib/components/Project.svelte';
 
-  let repos = [
-    {
-      url: 'abc.com',
-      name: 'annad2312/bru',
-      description: 'hi bois',
-      langColor: '#F4D35E',
-      langName: 'Python',
-      stars: 10
-    },
-    {
-      url: 'def.com',
-      name: 'annad2312/anoter',
-      description: 'wow',
-      langColor: '#F4D35E',
-      langName: 'Python',
-      stars: 12
-    }
-  ];
+  interface Repo {
+    url: string;
+    name: string;
+    description: string;
+    langColor: string;
+    langName: string;
+    stars: number;
+  }
+
+  export let repos: Array<Repo>;
 </script>
 
 <main>
   <Head favicon="favicon-3.webp" title="Projects" />
   <Navbar />
+  <center>
+    <p class="font-bold font-display text-pink-50 text-lg md:text-2xl mx-3 my-4 p-1">
+      Here are some of the projects I've worked on; these are the repositories that are pinned on my
+      GitHub profile.
+    </p>
+  </center>
   <div class="grid grid-cols-1 md:grid-cols-2 gap-2 m-3">
     {#each repos as repo}
       <Project {...repo} />
     {/each}
   </div>
-  <p class="m-3 font-bold font-display text-pink-50 text-lg md:text-2xl p-1">
-    ...and more on <a href="https://anand2312.tech/r/" class="text-lgreen-100 hover:text-pink-500"
-      >GitHub.</a
-    >
-  </p>
+  <center>
+    <p class="m-3 font-bold font-display text-pink-50 text-lg md:text-2xl p-1">
+      ...and more on <a href="https://anand2312.tech/r/" class="text-lgreen-100 hover:text-pink-500"
+        >GitHub.</a
+      >
+    </p>
+  </center>
 </main>
